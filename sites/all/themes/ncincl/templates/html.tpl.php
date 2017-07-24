522a501c859e441983303acecc10f207358e1161
Steve Mossner <smossner@gmail.com>
2017-07-24 12:14:16 -0400

    Added google analytics

--- sites/all/themes/ncincl/templates/html.tpl.php
+++ sites/all/themes/ncincl/templates/html.tpl.php
@@ -46,36 +46,64 @@
 <!-- @see http://foundation.zurb.com/docs/index.html#basicHTMLMarkup -->
 
 <!--[if IE 8]><html class="no-js lt-ie9" lang="<?php print $language->language ?>" dir="<?php print $language->dir ?>"> <![endif]-->
-<!--[if gt IE 8]><!--> <html class="no-js" lang="<?php print $language->language ?>" dir="<?php print $language->dir ?>"> <!--<![endif]-->
+<!--[if gt IE 8]><!-->
+<html class="no-js" lang="<?php print $language->language ?>" dir="<?php print $language->dir ?>">
+<!--<![endif]-->
+
 <head>
-  <script src="//assets.adobedtm.com/f1bfa9f7170c81b1a9a9ecdcc6c5215ee0b03c84/satelliteLib-4b219b82c4737db0e1797b6c511cf10c802c95cb.js"></script>
-  <?php print $head; ?>
-  <title><?php print $head_title; ?></title>
-  <link href='//fonts.googleapis.com/css?family=Lato:400,700,700italic,400italic' rel='stylesheet' type='text/css'>
-  <link href='//fonts.googleapis.com/css?family=Archivo+Narrow:400,400italic,700,700italic' rel='stylesheet' type='text/css'>
-  <link href='//fonts.googleapis.com/css?family=Montserrat:400,700' rel='stylesheet' type='text/css'>
-  <?php print $styles; ?>
-  <?php print $scripts; ?>
-  <!--[if lt IE 9]>
+	<script src="//assets.adobedtm.com/f1bfa9f7170c81b1a9a9ecdcc6c5215ee0b03c84/satelliteLib-4b219b82c4737db0e1797b6c511cf10c802c95cb.js"></script>
+	<?php print $head; ?>
+	<title>
+		<?php print $head_title; ?>
+	</title>
+	<link href='//fonts.googleapis.com/css?family=Lato:400,700,700italic,400italic' rel='stylesheet' type='text/css'>
+	<link href='//fonts.googleapis.com/css?family=Archivo+Narrow:400,400italic,700,700italic' rel='stylesheet' type='text/css'>
+	<link href='//fonts.googleapis.com/css?family=Montserrat:400,700' rel='stylesheet' type='text/css'>
+	<?php print $styles; ?>
+	<?php print $scripts; ?>
+	<!--[if lt IE 9]>
 		<script src="http://html5shiv.googlecode.com/svn/trunk/html5.js"></script>
 	<![endif]-->
 </head>
-<body class="<?php print $classes; ?>" <?php print $attributes;?>>
-  <div class="skip-link">
-    <a href="#main-content" class="element-invisible element-focusable"><?php print t('Skip to main content'); ?></a>
-  </div>
 
-  <?php print $page_top; ?>
-  <?php print $page; ?>
-  <?php print $page_bottom; ?>
-  <?php print _zurb_foundation_add_reveals(); ?>
-  <script>
-    (function ($, Drupal, window, document, undefined) {
-      $(document).foundation();
-    })(jQuery, Drupal, this, this.document);
-  </script>
-  
-<script type="text/javascript">_satellite.pageBottom();</script>
+<body class="<?php print $classes; ?>" <?php print $attributes;?>>
+	<div class="skip-link">
+		<a href="#main-content" class="element-invisible element-focusable">
+			<?php print t('Skip to main content'); ?>
+		</a>
+	</div>
+
+	<?php print $page_top; ?>
+	<?php print $page; ?>
+	<?php print $page_bottom; ?>
+	<?php print _zurb_foundation_add_reveals(); ?>
+	<script>
+		( function ( $, Drupal, window, document, undefined ) {
+			$( document ).foundation();
+		} )( jQuery, Drupal, this, this.document );
+	</script>
+
+	<script>
+		( function ( i, s, o, g, r, a, m ) {
+			i[ 'GoogleAnalyticsObject' ] = r;
+			i[ r ] = i[ r ] || function () {
+				( i[ r ].q = i[ r ].q || [] ).push( arguments )
+			}, i[ r ].l = 1 * new Date();
+			a = s.createElement( o ),
+				m = s.getElementsByTagName( o )[ 0 ];
+			a.async = 1;
+			a.src = g;
+			m.parentNode.insertBefore( a, m )
+		} )( window, document, 'script', 'https://www.google-analytics.com/analytics.js', 'ga' );
+
+		ga( 'create', 'UA-101867901-1', 'auto' );
+		ga( 'send', 'pageview' );
+	</script>
+
+	<script type="text/javascript">
+		_satellite.pageBottom();
+	</script>
 
 </body>
+
 </html>
