ca60f3f0c48ec5f4666a15a8b36bb837f8f729ca
Steve Mossner <smossner@gmail.com>
2019-03-04 10:42:39 -0500

    updated nci banner
    Updatd banner code to remove space

--- sites/all/themes/ncincl/templates/html.tpl.php
+++ sites/all/themes/ncincl/templates/html.tpl.php
@@ -51,6 +51,7 @@
 <!--<![endif]-->
 
 <head>
+<script src="//assets.adobedtm.com/f1bfa9f7170c81b1a9a9ecdcc6c5215ee0b03c84/satelliteLib-71a449a40c1fe9d8db22f15bc8fd2e3a768e8871.js"></script>
 <!-- Global site tag (gtag.js) - Google Analytics -->
 <script async src="https://www.googletagmanager.com/gtag/js?id=UA-101867901-1"></script>
 <script>
@@ -72,8 +73,6 @@
   })();
 
 </script>
-
-    <script src="//assets.adobedtm.com/f1bfa9f7170c81b1a9a9ecdcc6c5215ee0b03c84/satelliteLib-5b9e3ac44703470653bb9ed571575dc0232775ef.js"></script>
 	<?php print $head; ?>
 	<title>
 		<?php print $head_title; ?>
